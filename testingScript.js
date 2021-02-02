a7b57889c29146e5dd1e51fef001d7f10a41cec9
broy9387 <57662910+broy9387@users.noreply.github.com>
2021-02-02 16:23:36 +0530

    Add files via upload

---
+++ testingScript.js
@@ -0,0 +1,13 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyA9F6zriCDRHGeu-LwJ83CplHUKkQVCr0M",
+    authDomain: "corona-9e261.firebaseapp.com",
+    databaseURL: "https://corona-9e261.firebaseio.com",
+    projectId: "corona-9e261",
+    storageBucket: "corona-9e261.appspot.com",
+    messagingSenderId: "694830677499",
+    appId: "1:694830677499:web:b9382e363351f4b3701f5e"
+  };
+
+  firebase.initializeApp(firebaseConfig);
+  
+  
